fafdedd315117c6ecaff2558521d63a448890b26
zhangchangbin <zhangchangbin@sunline.cn>
2022-04-12 09:01:56 +0800

    优化工程结构

---
+++ compileflow-samples/src/main/java/com/alibaba/compileflow/SampleApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.compileflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangchangbin
+ * @description: TODO
+ * @date 2022/4/12 08:58
+ */
+@SpringBootApplication
+public class SampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class,args);
+    }
+}
